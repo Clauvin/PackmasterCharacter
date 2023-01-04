cf7954a6983d5674f3491ff67c5306e75c8689db
Cláuvin <Clauvin@users.noreply.github.com>
2023-01-04 11:59:43 -0300

    Created Forcefield.java

---
+++ src/main/java/thePackmaster/cards/royaltypack/Forcefield.java
@@ -0,0 +1,32 @@
+package thePackmaster.cards.royaltypack;
+
+import com.megacrit.cardcrawl.actions.common.DiscardAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import thePackmaster.cards.AbstractPackmasterCard;
+import thePackmaster.util.Wiz;
+
+import static thePackmaster.SpireAnniversary5Mod.makeID;
+
+public class Forcefield extends AbstractPackmasterCard {
+
+    public final static String ID = makeID("Forcefield");
+
+    public Forcefield() {
+        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
+        baseBlock = 8;
+        magicNumber = 16;
+    }
+
+    @Override
+    public void upp() {
+        baseBlock += 3;
+        magicNumber += 6;
+    }
+
+    @Override
+    public void use(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
+        Wiz.atb(new DiscardAction(abstractPlayer, abstractPlayer, 1, true));
+        //Create Action for TributeOrAusterity effect
+    }
+}
