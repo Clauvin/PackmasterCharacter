fbb968ff4fe47a19e7159b370f7400bc0c4d2dc8
Cláuvin <Clauvin@users.noreply.github.com>
2023-01-09 11:03:47 -0300

    [nobleform] LoseGoldPatch is done

---
+++ src/main/java/thePackmaster/patches/LoseGoldPatch.java
@@ -0,0 +1,39 @@
+package thePackmaster.patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.megacrit.cardcrawl.actions.GameActionManager;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.orbs.AbstractOrb;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+
+@SpirePatch(
+        clz = AbstractPlayer.class,
+        method = "loseGold"
+)
+public class LoseGoldPatch {
+    public interface OnLoseGold {
+        void onLoseGold(int goldAmount);
+    }
+
+    @SpirePostfixPatch
+    public static void loseGold(int goldAmount) {
+        if (!(AbstractDungeon.getCurrRoom() instanceof com.megacrit.cardcrawl.rooms.ShopRoom) &&
+                (AbstractDungeon.getCurrRoom()).phase != AbstractRoom.RoomPhase.COMBAT){
+            for (AbstractPower p : AbstractDungeon.player.powers) {
+                if (p instanceof OnLoseGold) {
+                    ((OnLoseGold) p).onLoseGold(goldAmount);
+                }
+            }
+            for (AbstractOrb orb : AbstractDungeon.player.orbs) {
+                if (orb instanceof OnLoseGold) {
+                    ((OnLoseGold) orb).onLoseGold(goldAmount);
+                }
+            }
+
+
+        }
+    }
+}
