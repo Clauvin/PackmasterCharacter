84caf36fc35ff8cbf905548385e1ebef3554a7f3
Cláuvin <Clauvin@users.noreply.github.com>
2023-01-13 11:32:25 -0300

    [noblestrike] Using DrawSpecificCardTypeAction

--- src/main/java/thePackmaster/cards/royaltypack/optioncards/NobleStrikeTribute.java
+++ src/main/java/thePackmaster/cards/royaltypack/optioncards/NobleStrikeTribute.java
@@ -1,11 +1,15 @@
 package thePackmaster.cards.royaltypack.optioncards;
 
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.unique.AttackFromDeckToHandAction;
 import com.megacrit.cardcrawl.actions.utility.ConditionalDrawAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import thePackmaster.actions.royaltypack.DrawSpecificCardTypeAction;
 import thePackmaster.cards.AbstractPackmasterCard;
 import thePackmaster.util.Wiz;
+
+import javax.smartcardio.Card;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
@@ -31,6 +35,9 @@ public class NobleStrikeTribute extends AbstractPackmasterCard {
 
     @Override
     public void onChoseThisOption(){
-        Wiz.atb(new ConditionalDrawAction(magicNumber, CardType.ATTACK));
+        //Add PayTributeAction
+        for (int i = 0; i < magicNumber; i++){
+            Wiz.atb(new DrawSpecificCardTypeAction(CardType.ATTACK));
+        }
     }
 }
