fd4d88be2bb03b6e242255f5617e5e0810813f3c
Cláuvin <Clauvin@users.noreply.github.com>
2023-01-17 13:35:44 -0300

    [boundlessTalent] In-progress Tribute option

---
+++ src/main/java/thePackmaster/cards/royaltypack/optioncards/BoundlessTalentTribute.java
@@ -0,0 +1,40 @@
+package thePackmaster.cards.royaltypack.optioncards;
+
+import com.megacrit.cardcrawl.actions.common.ObtainPotionAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import thePackmaster.actions.royaltypack.PayTributeAction;
+import thePackmaster.cards.AbstractPackmasterCard;
+import thePackmaster.util.Wiz;
+
+import static thePackmaster.SpireAnniversary5Mod.makeID;
+
+public class BoundlessTalentTribute extends AbstractPackmasterCard {
+
+    public final static String ID = makeID("BoundlessTalentTribute");
+    public final static int TRIBUTE_GOLD_AMOUNT = 5;
+
+    public BoundlessTalentTribute(){
+        super(ID, -2, CardType.STATUS, CardRarity.SPECIAL, CardTarget.SELF);
+    }
+
+    @Override
+    public void upp() {
+
+    }
+
+    @Override
+    public void use(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
+        onChoseThisOption();
+    }
+
+    @Override
+    public void onChoseThisOption() {
+        Wiz.atb(new PayTributeAction(TRIBUTE_GOLD_AMOUNT));
+
+    }
+
+
+
+}
