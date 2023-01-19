436f0e08a1632b68f2299442abfc128e4023e39b
Cláuvin <Clauvin@users.noreply.github.com>
2023-01-19 18:40:16 -0300

    [forcefield] And now it calls the modal option

--- src/main/java/thePackmaster/cards/royaltypack/Forcefield.java
+++ src/main/java/thePackmaster/cards/royaltypack/Forcefield.java
@@ -3,7 +3,10 @@ package thePackmaster.cards.royaltypack;
 import com.megacrit.cardcrawl.actions.common.DiscardAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import thePackmaster.actions.royaltypack.TributeOrAusterityAction;
 import thePackmaster.cards.AbstractPackmasterCard;
+import thePackmaster.cards.royaltypack.optioncards.BoundlessTalentAusterity;
+import thePackmaster.cards.royaltypack.optioncards.BoundlessTalentTribute;
 import thePackmaster.util.Wiz;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
@@ -27,6 +30,14 @@ public class Forcefield extends AbstractPackmasterCard {
     @Override
     public void use(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
         Wiz.atb(new DiscardAction(abstractPlayer, abstractPlayer, 1, true));
-        //Create Action for TributeOrAusterity effect
+
+        AbstractPackmasterCard forcefieldTributeChoiceCard = new ForcefieldTribute();
+        AbstractPackmasterCard forcefieldAusterityChoiceCard = new ForcefieldAusterity();
+        for (int i = 0; i < this.timesUpgraded; i++){
+            forcefieldTributeChoiceCard.upgrade();
+            forcefieldAusterityChoiceCard.upgrade();
+        }
+
+        Wiz.atb(new TributeOrAusterityAction(forcefieldTributeChoiceCard, forcefieldAusterityChoiceCard));
     }
 }
