a873787493251aea768c7ff7f13e5a9bd0536ece
Cláuvin <Clauvin@users.noreply.github.com>
2023-01-19 10:26:28 -0300

    [boundlessTalent] Ok, found and fixed the bug, now to make this work

--- src/main/java/thePackmaster/actions/royaltypack/BoundlessTalentAction.java
+++ src/main/java/thePackmaster/actions/royaltypack/BoundlessTalentAction.java
@@ -1,9 +1,11 @@
 package thePackmaster.actions.royaltypack;
 
+import basemod.BaseMod;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.powers.NightmarePower;
@@ -12,30 +14,46 @@ public class BoundlessTalentAction  extends AbstractGameAction {
 
     private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("CopyAction");
     private static final String[] TEXT = uiStrings.TEXT;
-    private static final float DURATION = 3;
+
+    public BoundlessTalentAction() {
+        this.duration = Settings.ACTION_DUR_FAST;
+        this.actionType = ActionType.WAIT;
+    }
 
     @Override
     public void update() {
-        if (this.duration == DURATION) {
+        if (this.duration == Settings.ACTION_DUR_FAST) {
             if (AbstractDungeon.player.hand.isEmpty()) {
+                BaseMod.logger.info("WHAT");
                 this.isDone = true;
             } else if (AbstractDungeon.player.hand.size() == 1) {
-                //Duplicate the only card
+                BaseMod.logger.info("WHAT 2");
+                if (!AbstractDungeon.handCardSelectScreen.wereCardsRetrieved) {
+                    AbstractCard tmpCard = AbstractDungeon.handCardSelectScreen.selectedCards.getBottomCard().
+                            makeStatEquivalentCopy();
+                    AbstractDungeon.player.hand.addToHand(tmpCard);
+                    AbstractDungeon.handCardSelectScreen.selectedCards.clear();
+                    AbstractDungeon.handCardSelectScreen.wereCardsRetrieved = true;
+                }
                 this.isDone = true;
             } else {
-                AbstractDungeon.handCardSelectScreen.open(TEXT[0], 1, false, false);
+                BaseMod.logger.info("YEAH");
+                AbstractDungeon.handCardSelectScreen.open("---", 1, false, false);
                 this.tickDuration();
             }
         } else {
+            BaseMod.logger.info("WHAT 3");
             if (!AbstractDungeon.handCardSelectScreen.wereCardsRetrieved) {
-                AbstractCard tmpCard = AbstractDungeon.handCardSelectScreen.selectedCards.getBottomCard().
-                        makeStatEquivalentCopy();
-                AbstractDungeon.player.hand.addToHand(tmpCard);
-                AbstractDungeon.handCardSelectScreen.selectedCards.clear();
-                AbstractDungeon.handCardSelectScreen.wereCardsRetrieved = true;
+                if (AbstractDungeon.handCardSelectScreen.selectedCards.size() != 0){
+                    AbstractCard tmpCard = AbstractDungeon.handCardSelectScreen.selectedCards.getBottomCard().
+                            makeStatEquivalentCopy();
+                    AbstractDungeon.player.hand.addToHand(tmpCard);
+                    AbstractDungeon.handCardSelectScreen.wereCardsRetrieved = false;
+                    AbstractDungeon.handCardSelectScreen.selectedCards.clear();
+                    BaseMod.logger.info("WHAT 4");
+                    this.isDone = true;
+                }
             }
-
-            this.tickDuration();
         }
     }
 }
