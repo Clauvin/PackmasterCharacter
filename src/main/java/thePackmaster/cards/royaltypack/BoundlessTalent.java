b1bfa6fd5a6d4b7f8c673b76c5f436212c640463
Cláuvin <Clauvin@users.noreply.github.com>
2023-01-19 09:40:33 -0300

    [boundlessTalent] now is Exhaustive 2 on upgrade

--- src/main/java/thePackmaster/cards/royaltypack/BoundlessTalent.java
+++ src/main/java/thePackmaster/cards/royaltypack/BoundlessTalent.java
@@ -1,5 +1,6 @@
 package thePackmaster.cards.royaltypack;
 
+import com.evacipated.cardcrawl.mod.stslib.variables.ExhaustiveVariable;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import thePackmaster.actions.royaltypack.TributeOrAusterityAction;
@@ -15,16 +16,20 @@ import static thePackmaster.SpireAnniversary5Mod.makeID;
 public class BoundlessTalent extends AbstractPackmasterCard {
 
     public final static String ID = makeID("BoundlessTalent");
+    private static final int EXHAUSTIVE = 2;
 
     public BoundlessTalent() {
         super(ID, 0, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.SELF);
         baseMagicNumber = 5;
         baseSecondMagic = 1;
+        this.exhaust = true;
     }
 
     @Override
     public void upp() {
         baseSecondMagic += 1;
+        this.exhaust = false;
+        ExhaustiveVariable.setBaseValue(this, EXHAUSTIVE);
     }
 
     @Override
