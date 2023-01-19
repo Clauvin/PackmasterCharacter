81e29f713002c6c1c48219b5ff37c87aaa63b2e7
Cláuvin <Clauvin@users.noreply.github.com>
2023-01-19 10:59:52 -0300

    [boundlessTalent] is working properly :D

--- src/main/java/thePackmaster/actions/royaltypack/BoundlessTalentAction.java
+++ src/main/java/thePackmaster/actions/royaltypack/BoundlessTalentAction.java
@@ -3,57 +3,65 @@ package thePackmaster.actions.royaltypack;
 import basemod.BaseMod;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainGoldAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.powers.NightmarePower;
+import thePackmaster.SpireAnniversary5Mod;
+import thePackmaster.util.Wiz;
 
 public class BoundlessTalentAction  extends AbstractGameAction {
 
-    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString("CopyAction");
-    private static final String[] TEXT = uiStrings.TEXT;
+    private static final String[] TEXT = CardCrawlGame.languagePack.getUIString(
+            SpireAnniversary5Mod.makeID("BoundlessTalentAction")).TEXT;
+    private int tributeCost;
 
-    public BoundlessTalentAction() {
+    public BoundlessTalentAction(int tributeCost) {
         this.duration = Settings.ACTION_DUR_FAST;
         this.actionType = ActionType.WAIT;
+        this.tributeCost = tributeCost;
     }
 
     @Override
     public void update() {
         if (this.duration == Settings.ACTION_DUR_FAST) {
             if (AbstractDungeon.player.hand.isEmpty()) {
-                BaseMod.logger.info("WHAT");
+                Wiz.atb(new GainGoldAction(tributeCost));
                 this.isDone = true;
             } else if (AbstractDungeon.player.hand.size() == 1) {
-                BaseMod.logger.info("WHAT 2");
-                if (!AbstractDungeon.handCardSelectScreen.wereCardsRetrieved) {
-                    AbstractCard tmpCard = AbstractDungeon.handCardSelectScreen.selectedCards.getBottomCard().
-                            makeStatEquivalentCopy();
-                    AbstractDungeon.player.hand.addToHand(tmpCard);
-                    AbstractDungeon.handCardSelectScreen.selectedCards.clear();
-                    AbstractDungeon.handCardSelectScreen.wereCardsRetrieved = true;
-                }
-                this.isDone = true;
+                doActionWithOneCardAtHand();
             } else {
-                BaseMod.logger.info("YEAH");
-                AbstractDungeon.handCardSelectScreen.open("---", 1, false, false);
+                AbstractDungeon.handCardSelectScreen.open(TEXT[0], 1, false, false);
                 this.tickDuration();
             }
         } else {
-            BaseMod.logger.info("WHAT 3");
-            if (!AbstractDungeon.handCardSelectScreen.wereCardsRetrieved) {
-                if (AbstractDungeon.handCardSelectScreen.selectedCards.size() != 0){
-                    AbstractCard tmpCard = AbstractDungeon.handCardSelectScreen.selectedCards.getBottomCard().
-                            makeStatEquivalentCopy();
-                    AbstractDungeon.player.hand.addToHand(tmpCard);
-                    AbstractDungeon.handCardSelectScreen.wereCardsRetrieved = false;
-                    AbstractDungeon.handCardSelectScreen.selectedCards.clear();
-                    BaseMod.logger.info("WHAT 4");
-                    this.isDone = true;
-                }
+            doAction();
+        }
+    }
+
+    private void doAction(){
+        if (!AbstractDungeon.handCardSelectScreen.wereCardsRetrieved) {
+            if (AbstractDungeon.handCardSelectScreen.selectedCards.size() != 0){
+                AbstractCard tmpCard = AbstractDungeon.handCardSelectScreen.selectedCards.getBottomCard().
+                        makeStatEquivalentCopy();
+                AbstractDungeon.player.hand.addToHand(AbstractDungeon.handCardSelectScreen.selectedCards.getBottomCard());
+                AbstractDungeon.player.hand.addToHand(tmpCard);
+                AbstractDungeon.handCardSelectScreen.wereCardsRetrieved = false;
+                AbstractDungeon.handCardSelectScreen.selectedCards.clear();
+                this.isDone = true;
             }
         }
+    }
+
+    private void doActionWithOneCardAtHand(){
+        AbstractCard tmpCard = AbstractDungeon.player.hand.getBottomCard().makeStatEquivalentCopy();
+        AbstractDungeon.player.hand.addToHand(AbstractDungeon.player.hand.getBottomCard());
+        AbstractDungeon.player.hand.addToHand(tmpCard);
+        AbstractDungeon.handCardSelectScreen.selectedCards.clear();
+        AbstractDungeon.handCardSelectScreen.wereCardsRetrieved = true;
+        this.isDone = true;
     }
 }
