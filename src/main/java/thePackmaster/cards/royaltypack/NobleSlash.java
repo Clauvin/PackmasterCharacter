8ad3970d67e95e32e3c0d38bafa451ba4a5a773b
Cláuvin <Clauvin@users.noreply.github.com>
2023-01-21 11:00:25 -0300

    [nobleSlash] Finished the card

--- src/main/java/thePackmaster/cards/royaltypack/NobleSlash.java
+++ src/main/java/thePackmaster/cards/royaltypack/NobleSlash.java
@@ -1,9 +1,11 @@
 package thePackmaster.cards.royaltypack;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DiscardAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import thePackmaster.cards.AbstractPackmasterCard;
+import thePackmaster.util.Wiz;
 
 import static thePackmaster.SpireAnniversary5Mod.makeID;
 
@@ -13,17 +15,17 @@ public class NobleSlash extends AbstractPackmasterCard {
 
     public NobleSlash(){
         super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
-        baseDamage = 12;
+        baseDamage = damage = 15;
     }
 
     @Override
     public void upp() {
-        baseDamage += 6;
+        upgradeDamage(5);
     }
 
     @Override
     public void use(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
-        //Apply debuff on player to discard card at the start of the next turn
+        Wiz.atb(new DiscardAction(abstractPlayer, abstractPlayer, 1, true));
         dmg(abstractMonster, AbstractGameAction.AttackEffect.SLASH_HEAVY);
     }
 }
