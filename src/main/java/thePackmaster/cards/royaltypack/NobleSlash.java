816f650a5de56b70739bb76bd96322e0f06a1e90
Cláuvin <Clauvin@users.noreply.github.com>
2023-01-23 19:24:38 -0300

    [nobleSlash] NERFED - now it also gives "Draw one less the next turn"

--- src/main/java/thePackmaster/cards/royaltypack/NobleSlash.java
+++ src/main/java/thePackmaster/cards/royaltypack/NobleSlash.java
@@ -1,9 +1,11 @@
 package thePackmaster.cards.royaltypack;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DiscardAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.DrawReductionPower;
 import thePackmaster.cards.AbstractPackmasterCard;
 import thePackmaster.util.Wiz;
 
@@ -26,6 +28,8 @@ public class NobleSlash extends AbstractPackmasterCard {
     @Override
     public void use(AbstractPlayer abstractPlayer, AbstractMonster abstractMonster) {
         Wiz.atb(new DiscardAction(abstractPlayer, abstractPlayer, 1, true));
+        Wiz.atb(new ApplyPowerAction(abstractPlayer, abstractPlayer,
+                new DrawReductionPower(abstractPlayer, 1)));
         dmg(abstractMonster, AbstractGameAction.AttackEffect.SLASH_HEAVY);
     }
 }
